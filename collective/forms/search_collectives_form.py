3382688acb054e8aa3c24e6c3eabe28f9af789d7
Ian Auld <imauld@gmail.com>
2017-02-19 18:28:51 -0800

    Giving up on uwsgi/nginx for now. Moving ahead with redis

--- collective/forms/search_collectives_form.py
+++ collective/forms/search_collectives_form.py
@@ -4,12 +4,14 @@ from django import forms
 
 from ..services import get_active_tags
 
+def get_choices():
+    return [(tag.pk, tag.name) for tag in get_active_tags()]
 
 class SearchCollectivesForm(forms.Form):
     name = forms.CharField(
         widget=forms.TextInput(attrs={"id": "name", "class": "form-control"})
     )
     tags = forms.MultipleChoiceField(
-        choices=[(tag.pk, tag.name) for tag in get_active_tags()],
+        choices=get_choices,
         widget=forms.SelectMultiple(attrs={'class': 'form-control'})
     )
